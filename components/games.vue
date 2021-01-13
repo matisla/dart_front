fba2eaf5070dbaa482855414ab1f42b101566e93
matisla <herrmann.matthieu@gmail.com>
2021-01-13 20:46:57 +0100

    initial commit

---
+++ components/games.vue
@@ -0,0 +1,34 @@
+<template>
+	<table class="table">
+		<thead>
+		<tr class="text-center">
+			<th>Player</th>
+			<th>Set</th>
+			<th>Leg</th>
+			<th>Score</th>
+		</tr>
+		</thead>
+		<tbody>
+		<tr class="text-center" v-for="player in users">
+			<td class=text-left>{{ player }}</td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		</tbody>
+	</table>
+</template>
+
+<script>
+export default {
+  name: 'score-board',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
